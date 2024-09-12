73310e01dabf90577123b09e029fa8397d56ac17
VHNGROUP <issvictornoguera@gmail.com>
2024-09-12 00:28:35 -0500

    Se agrega proyecto estrella en 2D y 3D

--- Estrella/main.py
+++ Estrella/main.py
@@ -1,34 +1,17 @@
-# import subprocess
 import asyncio
 import time
 
-# # async def main():
-# #     hilo1 = asyncio.create_task(subprocess.call(['python', 'estrella_Multiple.py']))
-# #     await asyncio.gather(hilo1)
-# async def tarea1():
-#     subprocess.call(['python', 'estrella_Multiple.py'])
-
-# async def tarea2():
-#     subprocess.call(['py', 'estrella_3D.py'])
-
-# async def main():
-#     task1 = asyncio.create_task(tarea1())
-#     task2 = asyncio.create_task(tarea2())
-#     await asyncio.gather(task1, task2)
-
-# asyncio.run(main())
-
-async def tarea1():
+async def EstrellaMultiple():
     process = await asyncio.create_subprocess_exec('py', 'estrella_Multiple.py')
     await process.wait()
 
-async def tarea2():
+async def Estrella3D():
     process = await asyncio.create_subprocess_exec('py', 'estrella_3D.py')
     await process.wait()
 
 async def main():
-    task1 = asyncio.create_task(tarea1())
-    task2 = asyncio.create_task(tarea2())
+    task1 = asyncio.create_task(EstrellaMultiple())
+    task2 = asyncio.create_task(Estrella3D())
     await asyncio.gather(task1, task2)
 
 asyncio.run(main())
